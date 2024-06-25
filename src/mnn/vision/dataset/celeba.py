7cf0d51901dddd4eb8482bb52c07ea111b2e6573
Emmanouil Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2024-06-25 23:50:48 +0300

    implement celeba dataset

--- src/mnn/vision/dataset/celeba.py
+++ src/mnn/vision/dataset/celeba.py
@@ -1,11 +1,27 @@
 import abc
 import pathlib
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 import cv2
 import torch.utils.data
 
 
-class BaseCelebaDataset(torch.utils.data.Dataset):
+def __read_image(path: pathlib.Path) -> torch.Tensor:
+    return cv2.imread(path.as_posix())
+
+
+def _clean_line(line: str) -> str:
+    return [x for x in line.strip().split(" ") if x != ""]
+
+
+def _load_image_as_tensor(image_path: pathlib.Path) -> torch.Tensor:
+    return torch.tensor(__read_image(image_path)).permute(2, 0, 1).float()
+
+
+def _annotation_as_tensor(annotation: List[int]) -> torch.Tensor:
+    return torch.tensor(annotation).float()
+
+
+class BaseCelebaDataset(torch.utils.data.Dataset, metaclass=abc.ABCMeta):
     def __init__(self, dataset_path: pathlib.Path, use_mini: bool = True) -> None:
         """
         Dataset structur:
@@ -36,24 +52,145 @@ class BaseCelebaDataset(torch.utils.data.Dataset):
         self.images_path = dataset_path / images_subdir
         self.images_paths = list(self.images_path.glob("*.jpg"))
 
-        self.annotations_path = dataset_path / self.__get_annotations_filename()
-        self.annotations = self.__parse_annotations()
+        self.annotations_path = dataset_path / self._get_annotations_filename()
+        self.annotations = self._parse_annotations(self.annotations_path)
 
     @abc.abstractmethod
-    def __get_annotations_filename(self) -> str:
+    def _get_annotations_filename(self) -> str:
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def __parse_annotations(self) -> Dict[str, List[int]]:
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        """
+        all CelebA datasets have the same structure following the formula:
+        image_name attribute_1 attribute_2 ... attribute_n
+        """
         raise NotImplementedError()
 
     def __len__(self):
         return len(self.images_paths)
 
-    def __getitem__(self, index) -> Any:
+    def __getitem__(self, index) -> Tuple[torch.Tensor, torch.Tensor]:
         image_path = self.images_paths[index]
-        image = self.__load_image(image_path)
-        annotation = self.annotations[image_path.name]
+        image = _load_image_as_tensor(image_path)
+        annotation = _annotation_as_tensor(self.annotations[image_path.name])
+        return image, annotation
 
-    def __load_image(self, image_path: pathlib.Path) -> torch.Tensor:
-        return torch.tensor(cv2.imread(image_path.as_posix())).permute(2, 0, 1).float()
+
+class CelebaIdentityDataset(BaseCelebaDataset):
+    def _get_annotations_filename(self) -> str:
+        return "identity_CelebA.txt"
+
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        annotations = {}
+        with annotations_path.open(mode="r") as f:
+            for line in f:
+                line_elements = _clean_line(line)
+                image_name, identity = line_elements[0], line_elements[1]
+                annotations[image_name] = [int(identity)]
+        return annotations
+
+
+class CelebaAttributesDataset(BaseCelebaDataset):
+    def _get_annotations_filename(self) -> str:
+        return "list_attr_celeba.txt"
+
+    def _set_attributes(self, attributes: List[str]) -> None:
+        self.attributes = attributes
+
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        annotations = {}
+        with annotations_path.open(mode="r") as f:
+            for i, line in enumerate(f):
+                if i == 1:
+                    self._set_attributes(_clean_line(line))
+                elif i > 1:
+                    line_elements = _clean_line(line)
+                    image_name = line_elements[0]
+                    attributes_values = [int(x) for x in line_elements[1:]]
+                    annotations[image_name] = attributes_values
+        return annotations
+
+
+class CelebaBBoxDataset(BaseCelebaDataset):
+    def _get_annotations_filename(self) -> str:
+        return "list_bbox_celeba.txt"
+
+    def _set_attributes(self, attributes: List[str]) -> None:
+        self.attributes = attributes
+
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        annotations = {}
+        with annotations_path.open(mode="r") as f:
+            for i, line in enumerate(f):
+                if i == 1:
+                    self._set_attributes(_clean_line(line))
+                if i > 1:
+                    line_elements = _clean_line(line)
+                    image_name = line_elements[0]
+                    bbox = [int(x) for x in line_elements[1:]]
+                    annotations[image_name] = bbox
+        return annotations
+
+
+class CelebaAlignedLandmarksDataset(BaseCelebaDataset):
+    def _get_annotations_filename(self) -> str:
+        return "list_landmarks_align_celeba.txt"
+
+    def _set_attributes(self, attributes: List[str]) -> None:
+        self.attributes = attributes
+
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        annotations = {}
+        with annotations_path.open(mode="r") as f:
+            for i, line in enumerate(f):
+                if i == 1:
+                    self._set_attributes(_clean_line(line))
+                if i > 1:
+                    line_elements = _clean_line(line)
+                    image_name = line_elements[0]
+                    landmarks = [int(x) for x in line_elements[1:]]
+                    annotations[image_name] = landmarks
+        return annotations
+
+
+class CelebaLandmarksDataset(BaseCelebaDataset):
+    def _get_annotations_filename(self) -> str:
+        return "list_landmarks_celeba.txt"
+
+    def _set_attributes(self, attributes: List[str]) -> None:
+        self.attributes = attributes
+
+    def _parse_annotations(
+        self, annotations_path: pathlib.Path
+    ) -> Dict[str, List[int]]:
+        annotations = {}
+        with annotations_path.open(mode="r") as f:
+            for i, line in enumerate(f):
+                if i == 1:
+                    self._set_attributes(_clean_line(line))
+                if i > 1:
+                    line_elements = _clean_line(line)
+                    image_name = line_elements[0]
+                    landmarks = [int(x) for x in line_elements[1:]]
+                    annotations[image_name] = landmarks
+        return annotations
+
+
+if __name__ == "__main__":
+    celeba = CelebaBBoxDataset(
+        pathlib.Path("/Users/emlvasilopoulos/Datasets/CelebA"), use_mini=True
+    )
+    img_tensor, annotation_tensor = celeba[1]
+    print(celeba.attributes)
+    print(annotation_tensor)
