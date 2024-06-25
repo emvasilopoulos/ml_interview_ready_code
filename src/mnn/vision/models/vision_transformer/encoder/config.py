e1b6d229b75397ebc8838fe3b949cde69c5262eb
Emmanouil Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2024-06-25 22:59:42 +0300

    add types and explain more

--- src/mnn/vision/models/vision_transformer/encoder/config.py
+++ src/mnn/vision/models/vision_transformer/encoder/config.py
@@ -5,7 +5,7 @@ import dataclasses
 class VisionTransformerEncoderConfiguration:
     use_cnn: bool
     patch_size: int = 32
-    number_of_layers: int = 1  # the number of sub-encoder-layers in the encoder
+    number_of_layers: int = 1  # the number of transformer encoder layers in the encoder
 
     # the number of expected features in the encoder/decoder inputs
     """
@@ -15,10 +15,14 @@ class VisionTransformerEncoderConfiguration:
     """
     d_model: int = 384
 
+    """
+    Multi-head attention allows the model to jointly attend to information
+    from different representation subspaces at different positions.
+    """
     n_heads: int = 16  # the number of heads in the multiheadattention models
 
     # Alterantive name --> hidden_dim
     feed_forward_dimensions: int = 512  # the dimension of the feedforward network model
-
-    eps = 1e-5  # the eps value in layer normalization components
-    bias = True  # whether to use bias in layernorm components
+    eps: int = 1e-5  # the eps value in final LayerNorm
+    bias: bool = True  # whether to use bias in layernorm components
+    mask_check: bool = False  # whether to check for mask in forward pass
