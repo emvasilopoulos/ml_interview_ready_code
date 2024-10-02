389054a441d0416470830207c185b5850da513c0
Manos <emmanouilvasilopoulos@gmail.com>
2024-10-03 00:21:21 +0300

    update configuration mapping

--- src/mnn/vision/models/vision_transformer/encoder/config.py
+++ src/mnn/vision/models/vision_transformer/encoder/config.py
@@ -3,6 +3,23 @@ from typing import Any, Dict
 
 
 @dataclasses.dataclass
+class LayerNormConfiguration:
+    has_layer_norm: bool
+    eps: int
+    elementwise_affine: bool
+    bias: bool
+
+    @staticmethod
+    def from_dict(model_configuration: Dict[str, Any]) -> "LayerNormConfiguration":
+        return LayerNormConfiguration(
+            has_layer_norm=model_configuration["has_layer_norm"],
+            eps=float(model_configuration["eps"]),
+            elementwise_affine=model_configuration["elementwise_affine"],
+            bias=model_configuration["bias"],
+        )
+
+
+@dataclasses.dataclass
 class VisionTransformerEncoderConfiguration:
     has_positional_encoding: bool
     is_input_to_positional_encoder_normalized: bool
@@ -22,15 +39,14 @@ class VisionTransformerEncoderConfiguration:
     Multi-head attention allows the model to jointly attend to information
     from different representation subspaces at different positions.
     """
-    n_heads: int = 16  #
+    n_heads: int
     # the activation function in the feedforward network model
-    activation: str = "gelu"
+    activation: str
     # Alterantive name --> hidden_dim
-    feed_forward_dimensions: int = 512  # the dimension of the feedforward network model
-    # the eps value in final LayerNorm
-    eps: int = 1e-5
-    # whether to use bias in layernorm components
-    bias: bool = True
+    feed_forward_dimensions: int  # the dimension of the feedforward network model
+
+    layer_norm_config: LayerNormConfiguration
+
     # whether to check for mask in forward pass
     mask_check: bool = False
 
@@ -39,16 +55,19 @@ class VisionTransformerEncoderConfiguration:
         model_configuration: Dict[str, Any]
     ) -> "VisionTransformerEncoderConfiguration":
         return VisionTransformerEncoderConfiguration(
-            model_configuration["has_positional_encoding"],
-            model_configuration["is_input_to_positional_encoder_normalized"],
-            model_configuration["number_of_layers"],
-            model_configuration["d_model"],
-            model_configuration["n_heads"],
-            model_configuration["activation"],
-            model_configuration["feed_forward_dimensions"],
-            float(model_configuration["eps"]),
-            model_configuration["bias"],
-            model_configuration["mask_check"],
+            has_positional_encoding=model_configuration["has_positional_encoding"],
+            is_input_to_positional_encoder_normalized=model_configuration[
+                "is_input_to_positional_encoder_normalized"
+            ],
+            number_of_layers=model_configuration["number_of_layers"],
+            d_model=model_configuration["d_model"],
+            n_heads=model_configuration["n_heads"],
+            activation=model_configuration["activation"],
+            feed_forward_dimensions=model_configuration["feed_forward_dimensions"],
+            layer_norm_config=LayerNormConfiguration.from_dict(
+                model_configuration["LayerNorm"]
+            ),
+            mask_check=model_configuration["mask_check"],
         )
 
 
