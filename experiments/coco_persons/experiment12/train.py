359fcdac2bbe1541769953648c5102afd7dc27ba
Manos <emmanouilvasilopoulos@gmail.com>
2024-10-08 22:09:38 +0300

    add new experiment (small model for fast training)

--- experiments/coco_persons/experiment12/train.py
+++ experiments/coco_persons/experiment12/train.py
@@ -1,16 +1,20 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-import mnn.vision.dataset.coco.training.train as coco_train
-from mnn.vision.dataset.coco.training.transform import BaseIOTransform
 import mnn.vision.image_size
-from mnn.vision.models.vision_transformer.e2e import DoubleRGBCombinator
+import mnn.vision.dataset.coco.training.train as coco_train
+import mnn.vision.dataset.utilities
+import mnn.vision.dataset.coco.loader
+from mnn.vision.dataset.coco.training.transform import BaseIOTransform
+from mnn.vision.models.vision_transformer.e2e import MyVisionTransformer, RGBCombinator
+from mnn.vision.models.vision_transformer.encoder.vit_encoder import (
+    RawVisionTransformerRGBEncoder,
+)
 from mnn.vision.models.vision_transformer.tasks.object_detection import (
     ObjectDetectionOrdinalHead,
 )
-import mnn.vision.dataset.utilities
 import mnn.vision.models.heads.object_detection
-import mnn.vision.dataset.coco.loader
+import mnn.vision.models.vision_transformer.encoder.utils as mnn_encoder_utils
 
 
 from mnn.vision.dataset.coco.training.utils import *
@@ -31,12 +35,54 @@ class VitObjectDetectionNetwork(torch.nn.Module):
         self.expected_image_size = mnn.vision.image_size.ImageSize(
             width=expected_image_width, height=expected_image_height
         )
-        self.encoder = DoubleRGBCombinator(model_config, self.expected_image_size)
-        self.head = ObjectDetectionOrdinalHead(config=head_config)
+
+        combinator_activation = mnn_encoder_utils.get_combinator_activation_from_config(
+            model_config.rgb_combinator_config
+        )
+        self.rgb_combinator = RGBCombinator(
+            encoder=RawVisionTransformerRGBEncoder(
+                model_config.rgb_combinator_config,
+                self.expected_image_size,
+            ),
+            combinator_activation=combinator_activation,
+        )
+        self.hidden_transformer0 = (
+            mnn_encoder_utils.get_transformer_encoder_from_config(
+                model_config.encoder_config
+            )
+        )
+        self.hidden_transformer1 = (
+            mnn_encoder_utils.get_transformer_encoder_from_config(head_config)
+        )
+
+        layer_norm_eps = model_config.encoder_config.layer_norm_config.eps
+        bias = model_config.encoder_config.layer_norm_config.bias
+        self.layer_norm0 = torch.nn.LayerNorm(
+            model_config.encoder_config.d_model, eps=layer_norm_eps, bias=bias
+        )
+        self.layer_norm1 = torch.nn.LayerNorm(
+            model_config.encoder_config.d_model, eps=layer_norm_eps, bias=bias
+        )
+
+        self.layer_norm2 = torch.nn.LayerNorm(
+            model_config.encoder_config.d_model, eps=layer_norm_eps, bias=bias
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.encoder(x)
-        return self.head(x)
+        x_mean_ch = x.mean(dim=1)
+        x0 = self.rgb_combinator(x)
+        x_ = self.layer_norm0(x0 + x_mean_ch)  # Residual
+        # print(f"Layer norm-0: {x_.min()}, {x_.max()}")
+
+        x1 = self.hidden_transformer0(x0)
+        x_ = self.layer_norm1(x1 + x_ + x_mean_ch)  # Residual
+        # print(f"Layer norm-1: {x_.min()}, {x_.max()}")
+
+        x2 = self.hidden_transformer1(x1)
+        x_ = self.layer_norm2(x_mean_ch + x0 + x1 + x2)  # Residual
+        # print(f"Layer norm-2: {x_.min()}, {x_.max()}")
+
+        return x_  # possibly return x0, x1, x2
 
 
 class IOTransform(BaseIOTransform):
@@ -101,6 +147,7 @@ if __name__ == "__main__":
     writer = SummaryWriter(log_dir=f"runs/{experiment}_coco_my_vit_normed_predictions")
     print("- Open tensorboard with:\ntensorboard --logdir=runs")
 
+    save_dir = pathlib.Path("trained_models")
     coco_train.train_val(
         dataset_dir=dataset_dir,
         object_detection_model=object_detection_model,
@@ -113,4 +160,5 @@ if __name__ == "__main__":
         io_transform=IOTransform(),
         prediction_transform=IOTransform(),
         log_rate=1000,
+        save_dir=save_dir,
     )
