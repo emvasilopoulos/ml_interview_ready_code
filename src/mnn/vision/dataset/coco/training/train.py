6b3d6e23c9ef440719f54feb29e61f8573853ed7
Manos <emmanouilvasilopoulos@gmail.com>
2024-10-14 21:55:54 +0300

    add scheduler for optimizer

--- src/mnn/vision/dataset/coco/training/train.py
+++ src/mnn/vision/dataset/coco/training/train.py
@@ -1,14 +1,13 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from mnn.vision.dataset.coco.training.transform import BaseIOTransform
 import mnn.vision.image_size
-from mnn.vision.models.vision_transformer.e2e import MyVisionTransformer
 import mnn.vision.dataset.utilities
 import mnn.vision.models.heads.object_detection
 import mnn.vision.dataset.coco.loader
 
 
+from mnn.vision.dataset.coco.training.transform import BaseIOTransform
 from mnn.vision.dataset.coco.training.utils import *
 from mnn.vision.dataset.coco.training.session import train_one_epoch, val_once
 
@@ -43,12 +42,10 @@ def train_val(
         validation_image_path, object_detection_model.expected_image_size
     ).to(device, dtype=hyperparameters_config.floating_point_precision)
     temp_out = object_detection_model(validation_image)
-    write_validation_image_with_predicted_mask(
-        temp_out, validation_image, f"epoch_{0}_id_test"
-    )
+    write_image_with_mask(temp_out, validation_image, "pre-session_prediction")
 
     # See coco["categories"]
-    classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 88]
+    classes = [1]
     train_dataset = mnn.vision.dataset.coco.loader.COCODatasetInstances2017(
         dataset_dir,
         "train",
@@ -77,6 +74,7 @@ def train_val(
     if not save_dir.exists():
         save_dir.mkdir(parents=True)
     model_save_path = save_dir / f"{experiment}_object_detection.pth"
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
     for epoch in range(hyperparameters_config.epochs):
         print(f"---------- EPOCH-{epoch} ------------")
         train_one_epoch(
@@ -95,15 +93,14 @@ def train_val(
             model_save_path=model_save_path,
         )
         torch.save(object_detection_model.state_dict(), model_save_path)
-        val_once(
+        val_loss = val_once(
             val_loader,
             object_detection_model,
             loss_fn,
             hyperparameters_config,
             epoch,
-            io_transform=io_transform,
-            prediction_transform=prediction_transform,
             device=device,
             writer=writer,
             log_rate=log_rate,
         )
+        scheduler.step(val_loss)
